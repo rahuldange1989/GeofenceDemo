483eb2379469535149de3c90ef4ba52b0703593d
Rahul Dange <rahul@inglab.com.my>
2019-05-02 17:13:20 +0800

    Initial Commit

---
+++ GeofenceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeofenceDemo
+//
+//  Created by Rahul Dange on 5/2/19.
+//  Copyright © 2019 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
